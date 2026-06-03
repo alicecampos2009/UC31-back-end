bae964c8a26e284c604ec0fd87c91189a11203a7
Alice Campos <alicecarolinacc271@gmail.com>
2026-06-03 08:27:13 -0300

    Aula do dia 02.06 e 03.06

--- mainCadastro.py
+++ mainCadastro.py
@@ -15,5 +15,23 @@ def cadastro():
             mensagem = f"Cadastro realizado com sucesso! Bem-vindo, {nome}"
     return render_template('cadastro.html', mensagem=mensagem)
 
+
+@app.route('/')
+def formulario():
+    return render_template (cadastro.html)
+
+@app.route('/validação', methods=['POST'])
+def cadastro():
+
+    nome = request.form.get('nome', '').strip().title()
+    email = request.form.get('email', '').strip().lower()
+    cidade = request.form.get('cidade', '').strip().title()
+
+    return f"""
+    Nome: {nome} <br>
+    Email: {email} <br>
+    Cidade: {cidade} <br>
+    """
+
 if __name__ == '__main__':
     app.run(debug=True) 
